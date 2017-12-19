e6894484b3c25843d756c83048a9695af8573b27
lewis520lys@163.com <346818513a>
2017-12-19 14:21:24 +0800

    first commit

---
+++ app/src/main/java/com/xdkj/xd/xdtools/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xdkj.xd.xdtools;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhy.autolayout.AutoLayoutActivity;
+
+public class MainActivity extends AutoLayoutActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
